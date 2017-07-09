43a62410d7d82c0c807eb5c033efb9ce449f658e
s890218 <wl00882105@gmail.com>
2017-07-09 22:29:17 +0800

    專案上傳

---
+++ Assets/CamLookPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLookPlayer : MonoBehaviour {
+
+    public Transform m_Player = null;
+    public Transform m_Cam = null;
+    private float m_PosY = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float math_f = Mathf.Lerp(m_Cam.localPosition.y, m_PosY, 0.1f);
+        m_Cam.localPosition = new Vector3(m_Player.localPosition.x, math_f, -10);
+        m_PosY = m_Player.localPosition.y;
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform m_Player = null;
+    public Rigidbody2D m_RigidbodyPlayer = null;
+    public Transform m_Left = null;
+    public Transform m_Right = null;
+
+    private void Awake()
+    {
+        m_Player.gameObject.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            m_Player.rotation = m_Left.rotation;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_RigidbodyPlayer.AddForce(new Vector2(-500, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_Player.rotation = m_Right.rotation;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_RigidbodyPlayer.AddForce(new Vector2(500, 0));
+        }
+        RaycastHit2D ray = Physics2D.Raycast(m_Player.transform.localPosition, Vector2.down,1.8f,1<<12);
+        if(ray.collider)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_RigidbodyPlayer.AddForce(new Vector2(0, 2300));
+            }
+        }
+        if(m_Player.transform.localPosition.y<-10)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
