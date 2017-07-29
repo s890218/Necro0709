0be8463fa9288e68c1755d2d069805af85ae6c13
BILLY_CJSCOPE\呆呆 <playbilly358@gmail.com>
2017-07-30 01:20:42 +0800

    0
    二段跳完成

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D m_RigidbodyPlayer = null;
     public Transform m_Left = null;
     public Transform m_Right = null;
+    public bool _canTwoJump = false;
+
     Quaternion Leftt;
     Quaternion Rightt;
     Animator _objAnimator;
@@ -33,13 +35,13 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         _objAnimator.SetFloat("Speed", Input.GetAxis("Horizontal"));
-
         
+
         if (Input.GetAxis("Horizontal") == 0 && !Input.GetButton("Horizontal"))
         {
             _objAnimator.SetBool("isIdle", true);
             _objAnimator.speed = 1;
-            return;
+            
             
             //Debug.Log(Input.GetAxis("Horizontal"));
         }
@@ -47,8 +49,8 @@ public class PlayerController : MonoBehaviour {
         {
             m_Player.rotation = Rightt;
             _objAnimator.SetBool("isIdle", false);
-            _objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f;
-            return;
+            //_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f;
+            
 
             //Debug.Log(Input.GetAxis("Horizontal"));
         }
@@ -56,18 +58,18 @@ public class PlayerController : MonoBehaviour {
         {
             m_Player.rotation = Leftt;
             _objAnimator.SetBool("isIdle", false);
-            _objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f* -1;
-            return;
+            //_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f* -1;
+            
 
             //Debug.Log(Input.GetAxis("Horizontal"));
         }
-       /* 
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            _objAnimator.SetBool("isIdle", false);
-            return;
-        }
-        */
+        /* 
+         if (Input.GetAxis("Horizontal") != 0)
+         {
+             _objAnimator.SetBool("isIdle", false);
+             return;
+         }
+         */
         /*
 		if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -76,36 +78,47 @@ public class PlayerController : MonoBehaviour {
             
 
         }
-        */
+        
         if (Input.GetKey(KeyCode.A))
         {
             m_RigidbodyPlayer.AddForce(new Vector2(-500, 0));
         }
-        /*
+        
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             //m_Player.rotation = m_Right.rotation;
             m_Player.rotation = Rightt;
         }
-        */
+        
         if (Input.GetKey(KeyCode.D))
         {
             m_RigidbodyPlayer.AddForce(new Vector2(500, 0));
         }
-
-        RaycastHit2D ray = Physics2D.Raycast(m_Player.transform.localPosition, Vector2.down,1.8f,1<<12);
+        */
+        RaycastHit2D ray = Physics2D.Raycast(m_Player.transform.localPosition, Vector2.down, 1.8f, 1 << 12);
         if (ray.collider)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetButtonDown("Fire1"))
             {
                 m_RigidbodyPlayer.AddForce(new Vector2(0, 2300));
                 Debug.Log("777777");
             }
+            else { _canTwoJump = true;return; }
+        }
+        else if (  _canTwoJump == true && !ray.collider && Input.GetButtonDown("Fire1"))
+        {
+            
+            m_RigidbodyPlayer.velocity = new Vector2(m_RigidbodyPlayer.velocity.x, 0f);
+            m_RigidbodyPlayer.AddForce(new Vector2(0, 2300));
+            _canTwoJump = false;
+
         }
 
-        if(m_Player.transform.localPosition.y<-10)
+
+        if (m_Player.transform.localPosition.y<-10)
         {
             SceneManager.LoadScene(0);
         }
     }
+
 }
