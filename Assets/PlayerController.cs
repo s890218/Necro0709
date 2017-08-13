b33623b01d59d8f286ec1481e9ee0d78be86d1cb
s890218 <wl00882105@gmail.com>
2017-08-13 19:05:16 +0800

    控制器上傳一版

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
-    public Transform m_Player = null;
+	[SerializeField]
+	private Transform m_BottomPoint1 = null;
+	[SerializeField]
+	private Transform m_BottomPoint2 = null;
+
+	public Transform m_Player = null;
     public Rigidbody2D m_RigidbodyPlayer = null;
     public Transform m_Left = null;
     public Transform m_Right = null;
@@ -15,13 +20,23 @@ public class PlayerController : MonoBehaviour {
     Quaternion Rightt;
     Animator _objAnimator;
 
-    private void Awake()
+	private Vector2 m_check1;
+	private Vector2 m_check2;
+	private float m_Dis1;
+	private float m_Dis2;
+	private bool m_IsLeft = true;
+
+	private void Awake()
     {
         m_Player.gameObject.SetActive(true);
         Leftt = Quaternion.Euler(0, 0, 0);
         Rightt = Quaternion.Euler(0, 180, 0);
         _objAnimator = m_Player.gameObject.GetComponent<Animator>();
-    }
+		m_check1 = new Vector2(m_BottomPoint1.position.x - m_Player.position.x, m_BottomPoint1.position.y - m_Player.position.y );
+		m_check2 = new Vector2(m_BottomPoint2.position.x - m_Player.position.x, m_BottomPoint2.position.y - m_Player.position.y);
+		m_Dis1 = Vector3.Distance(m_Player.transform.localPosition, m_BottomPoint1.position);
+		m_Dis2 = Vector3.Distance(m_Player.transform.localPosition, m_BottomPoint2.position);
+	}
 
     // Use this for initialization
     void Start ()
@@ -49,28 +64,28 @@ public class PlayerController : MonoBehaviour {
         {
             m_Player.rotation = Rightt;
             _objAnimator.SetBool("isIdle", false);
-            //_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f;
-            
+			//_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f;
+			RotatePlayerDis(false);
 
-            //Debug.Log(Input.GetAxis("Horizontal"));
-        }
+			//Debug.Log(Input.GetAxis("Horizontal"));
+		}
         else if (Input.GetAxis("Horizontal") < 0)
         {
             m_Player.rotation = Leftt;
             _objAnimator.SetBool("isIdle", false);
-            //_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f* -1;
-            
+			//_objAnimator.speed = Input.GetAxis("Horizontal") * 1.2f* -1;
+			RotatePlayerDis(true);
 
-            //Debug.Log(Input.GetAxis("Horizontal"));
-        }
-        /* 
+			//Debug.Log(Input.GetAxis("Horizontal"));
+		}
+		/* 
          if (Input.GetAxis("Horizontal") != 0)
          {
              _objAnimator.SetBool("isIdle", false);
              return;
          }
          */
-        /*
+		/*
 		if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             //m_Player.rotation = m_Left.rotation;
@@ -95,17 +110,22 @@ public class PlayerController : MonoBehaviour {
             m_RigidbodyPlayer.AddForce(new Vector2(500, 0));
         }
         */
-        RaycastHit2D ray = Physics2D.Raycast(m_Player.transform.localPosition, Vector2.down, 1.8f, 1 << 12);
-        if (ray.collider)
+#if UNITY_EDITOR
+		Debug.DrawRay(m_Player.position, m_check1, Color.red, m_Dis1);
+		Debug.DrawRay(m_Player.position, m_check2, Color.red, m_Dis2);
+#endif
+		RaycastHit2D ray = Physics2D.Raycast(m_Player.position, m_check1,m_Dis1, 1 << 12);
+		RaycastHit2D ray2 = Physics2D.Raycast(m_Player.position, m_check2, m_Dis2, 1 << 12);
+
+		if (ray.collider|| ray2.collider)
         {
             if (Input.GetButtonDown("Fire1"))
             {
                 m_RigidbodyPlayer.AddForce(new Vector2(0, 2300));
-                Debug.Log("777777");
             }
             else { _canTwoJump = true;return; }
         }
-        else if (  _canTwoJump == true && !ray.collider && Input.GetButtonDown("Fire1"))
+        else if (  _canTwoJump == true && !(ray.collider|| ray2.collider) && Input.GetButtonDown("Fire1"))
         {
             
             m_RigidbodyPlayer.velocity = new Vector2(m_RigidbodyPlayer.velocity.x, 0f);
@@ -121,4 +141,15 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+	private void RotatePlayerDis(bool isLeft)
+	{
+		if(m_IsLeft == isLeft)
+		{
+			return;
+		}
+		m_IsLeft = isLeft;
+		Vector2 v = m_check1;
+		m_check1 = new Vector2(-1* m_check1.x, m_check1.y);
+		m_check2 = new Vector2(-1 * m_check2.x, m_check2.y);
+	}
 }
